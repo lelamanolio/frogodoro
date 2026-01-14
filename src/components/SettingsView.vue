50b431e0d1f0dc2f5329d848dad4ddd63f49d44a
Micaela Manolio <micaela.manolio@gmail.com>
2026-01-14 17:53:17 +0100

    ✨ manage button group to change sound type

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -5,35 +5,65 @@
     <ul>
       <li>Focus Count -> number</li>
       <li>
-        <InputNumber :number="settings.focusCount" :step="1" @number-change="val => updateSettings('focusCount', val)" />
+        <InputNumber
+          :number="settings.focusCount"
+          :step="1"
+          @number-change="(val) => updateSettings('focusCount', val)"
+        />
       </li>
       <li>Focus Time -> minutes</li>
-        <InputNumber :number="settings.focusTime" :step="5" @number-change="val => updateSettings('focusTime', val)" />
+      <InputNumber
+        :number="settings.focusTime"
+        :step="5"
+        @number-change="(val) => updateSettings('focusTime', val)"
+      />
       <li>Short Break Time -> minutes</li>
-        <InputNumber :number="settings.shortBreakTime" :step="5" @number-change="val => updateSettings('shortBreakTime', val)" />
+      <InputNumber
+        :number="settings.shortBreakTime"
+        :step="5"
+        @number-change="(val) => updateSettings('shortBreakTime', val)"
+      />
       <li>Long Break -> on/off</li>
-        <ToggleSwitch :value="settings.longBreak" @toggle-change="val => updateSettings('longBreak', val)" />
+      <ToggleSwitch
+        :value="settings.longBreak"
+        @toggle-change="(val) => updateSettings('longBreak', val)"
+      />
       <li>Long Break Time -> minutes</li>
-        <InputNumber :class="settings.longBreak === false ? 'c-number--disabled' : ''" :number="settings.longBreakTime" :step="5" @number-change="val => updateSettings('longBreakTime', val)" />
+      <InputNumber
+        :class="settings.longBreak === false ? 'c-number--disabled' : ''"
+        :number="settings.longBreakTime"
+        :step="5"
+        @number-change="(val) => updateSettings('longBreakTime', val)"
+      />
       <li>Sounds -> on/off</li>
-        <ToggleSwitch :value="settings.sounds" @toggle-change="val => updateSettings('sounds', val)" />
-      <li>Sounds > -> lofi / white noises</li>
-        <ButtonGroup />
+      <ToggleSwitch
+        :value="settings.sounds"
+        @toggle-change="(val) => updateSettings('sounds', val)"
+      />
+      <li>Sounds > -> lofi / rain / fire</li>
+      <ButtonGroup :group="'sounds'" :buttons="['lofi', 'rain', 'fire']" @sound-change="(val) => updateSettings('soundType', val)" :value="settings.soundType" />
       <li>Sound volume -> slider (range input)</li>
       <li>Use system theme -> on/off</li>
-        <ToggleSwitch :value="settings.useSystemTheme" @toggle-change="val => updateSettings('useSystemTheme', val)" />
+      <ToggleSwitch
+        :value="settings.useSystemTheme"
+        @toggle-change="(val) => updateSettings('useSystemTheme', val)"
+      />
       <li>Dark mode -> on/off</li>
-        <ToggleSwitch :class="settings.useSystemTheme === true ? 'c-toggle--disabled' : ''" :value="settings.darkMode" @toggle-change="val => updateSettings('darkMode', val)" />
+      <ToggleSwitch
+        :class="settings.useSystemTheme === true ? 'c-toggle--disabled' : ''"
+        :value="settings.darkMode"
+        @toggle-change="(val) => updateSettings('darkMode', val)"
+      />
       <li>About -> developer, Heroicons, app version</li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue';
-import ButtonGroup from './form/ButtonGroup.vue';
-import InputNumber from './form/InputNumber.vue';
-import ToggleSwitch from './form/ToggleSwitch.vue';
+import { ref, onBeforeMount } from 'vue'
+import ButtonGroup from './form/ButtonGroup.vue'
+import InputNumber from './form/InputNumber.vue'
+import ToggleSwitch from './form/ToggleSwitch.vue'
 
 const settings = ref({
   focusCount: 4,
@@ -45,33 +75,31 @@ const settings = ref({
   soundType: 'lofi',
   volume: 40,
   useSystemTheme: true,
-  darkMode: false
-});
-const storageKey = 'frogodoro.settings';
+  darkMode: false,
+})
+const storageKey = 'frogodoro.settings'
 
 const setCustomSettings = () => {
-  localStorage.setItem(storageKey, JSON.stringify(settings.value));
+  localStorage.setItem(storageKey, JSON.stringify(settings.value))
 }
 
 const updateSettings = (key, value) => {
-  settings.value[key] = value;
+  settings.value[key] = value
 
-  setCustomSettings();
+  setCustomSettings()
 }
 
 onBeforeMount(() => {
   try {
-    const raw = localStorage.getItem(storageKey);
+    const raw = localStorage.getItem(storageKey)
 
     if (raw) {
-      settings.value = JSON.parse(raw);
+      settings.value = JSON.parse(raw)
     }
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 })
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- src/components/form/ButtonGroup.vue
+++ src/components/form/ButtonGroup.vue
@@ -1,20 +1,66 @@
 <template>
-  <div>
-    <label>
-      <input type="radio" name="sounds" />
-      Lofi
-    </label>
-    <label>
-      <input type="radio" name="sounds" />
-      White noises
-    </label>
+  <div class="c-button-group">
+    <template v-for="button of props.buttons">
+      <input class="c-button-group__input" type="radio" :value="button" :id="button" :name="props.group" :checked="props.value === button" @change="e => emit('soundChange', e.target.value)" />
+      <label class="c-button-group__label" :for="button">
+        {{ button }}
+      </label>
+    </template>
   </div>
 </template>
 
 <script setup>
+import { defineProps, defineEmits } from 'vue';
 
+const emit = defineEmits(['soundChange']);
+const props = defineProps({
+  group: {
+    type: String,
+  },
+  buttons: {
+    type: Array,
+  },
+  value: {
+    type: String
+  }
+});
 </script>
 
 <style lang="scss">
+.c-button-group {
+  display: flex;
+}
 
+.c-button-group__label {
+  padding: 6px 12px;
+  cursor: pointer;
+  border: 1px solid var(--text-muted);
+  transition: border 0.1s linear, background-color 0.1s linear;
+
+  &:hover {
+    border: 1px solid var(--text);
+    background-color: color-mix(in srgb, var(--background) 99%, var(--text) 1%);
+  }
+
+  &:first-of-type {
+    border-top-left-radius: 12px;
+    border-bottom-left-radius: 12px;
+  }
+
+  &:last-of-type {
+    border-top-right-radius: 12px;
+    border-bottom-right-radius: 12px;
+  }
+}
+
+.c-button-group__input {
+  opacity: 0;
+  position: absolute;
+  visibility: hidden;
+
+  &:checked + .c-button-group__label {
+    background-color: var(--primary-color);
+    color: var(--background);
+  }
+}
 </style>
