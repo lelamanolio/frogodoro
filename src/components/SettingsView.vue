63b9640dba79f780cff8bdbe6a2ba1b3a8d658ae
Micaela Manolio <micaela.manolio@gmail.com>
2026-01-15 09:18:32 +0100

    ✨ add basic modal + manage show/hide

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <section class="c-settings">
     Settings
     <ul>
       <li>Focus Count -> number</li>
@@ -42,7 +42,7 @@
       <li>Sounds > -> lofi / rain / fire</li>
       <ButtonGroup
         :group="'sounds'"
-        :buttons="['lofi', 'rain', 'fire']"
+        :buttons="['rain', 'fire', 'white noise']"
         @sound-change="(val) => settingsStore.updateSettings('soundType', val)"
         :value="settings.soundType"
       />
@@ -63,19 +63,36 @@
         @toggle-change="(val) => settingsStore.updateSettings('darkMode', val)"
       />
       <li>About -> developer, Heroicons, app version</li>
+      <li>
+        <button class="c-settings__about" @click="modalShown = true">
+          <InfoIcon />
+          <span>About</span>
+        </button>
+      </li>
     </ul>
+  </section>
+
+  <div class="c-modal" :class="{ 'c-modal--show' : modalShown }">
+    <button @click="modalShown = false"><CloseIcon /></button>
+    <p>Developed by: <a href="https://lela.codes/">lela</a></p>
+    <p>Icons by: <a href="https://heroicons.com/outline">Heroicons</a></p>
+    <p>Sounds by: <a href="https://freesound.org/people/_lynks/sounds/595717/">rain</a>, <a href="https://freesound.org/people/Nox_Sound/sounds/472591/">fire</a> & <a href="https://freesound.org/people/DJT4NN3R/sounds/347576/">white noise</a></p>
   </div>
 </template>
 
 <script setup>
-import { onBeforeMount, computed } from 'vue'
+import { onBeforeMount, computed, ref } from 'vue'
 import { useSettings } from '../composables/useSettings.js'
 import ButtonGroup from './form/ButtonGroup.vue'
 import InputNumber from './form/InputNumber.vue'
 import ToggleSwitch from './form/ToggleSwitch.vue'
 import VolumeSlider from './form/VolumeSlider.vue'
+import InfoIcon from './icons/InfoIcon.vue'
+import CloseIcon from './icons/CloseIcon.vue'
 
 const settingsStore = useSettings();
+
+const modalShown = ref(false);
 
 const settings = computed(() => {
   return settingsStore.settings.value
@@ -85,3 +102,22 @@ onBeforeMount(() => {
   settingsStore.loadSettings()
 });
 </script>
+
+<style lang="scss">
+.c-settings__about {
+  cursor: pointer;
+}
+
+.c-modal {
+  display: none;
+  position: fixed;
+  background-color: var(--background);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  &.c-modal--show {
+    display: block;
+  }
+}
+</style>
---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="c-icon c-icon--close"
+    :style="cssProps"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+  </svg>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: '#000',
+  },
+  size: {
+    type: Number,
+    default: 24,
+  },
+})
+
+const cssProps = computed(() => {
+  return {
+    '--icon-color': `${props.color}`,
+    '--icon-width': `${props.size}px`,
+  }
+})
+</script>
